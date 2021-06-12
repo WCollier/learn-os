3567239abe82a173955ea9eba0a09f4c5a4771ea
WCollier <me@williamcollier.me.uk>
2021-06-12 20:04:54 +0100

    Move writer out into it's own type, Terminal should now be stateless

--- src/Terminal.zig
+++ src/Terminal.zig
@@ -14,26 +14,48 @@ const VGA_HEIGHT = Vga.VGA_HEIGHT;
 
 const VGA_SIZE = Vga.VGA_SIZE;
 
-pub const Terminal = struct {
-    current_colour: ColourCode = ColourCode.init(.light_grey, .black),
+fn TerminalWriter() type {
+    return struct {
+        const Error = error{};
 
-    buffer: []ScreenChar,
+        const Writer = std.io.Writer(*Self, Error, write); 
+
+        const Self = @This();
+
+        fn write(self: *Self, bytes: []const u8) Error!usize {
+            Terminal.write(bytes);
+
+            return bytes.len;
+        }
+
+        pub fn writer(self: *Self) Writer {
+            return .{ .context = self };
+        }
+
+        pub fn log(self: *Self, msg: []const u8) void {
+            self.writer().writeAll(msg) catch |e| switch (e) {};
+
+            Terminal.returnLine();
+        }
+
+        pub fn print(self: *Self, comptime msg: []const u8, args: anytype) void {
+            self.writer().print(msg, args) catch |e| switch (e) {};
+        }
+    };
+}
+
+pub fn terminalWriter() TerminalWriter() {
+    return .{};
+}
+
+pub const Terminal = struct {
+    const buffer = @intToPtr([*]volatile ScreenChar, 0xB8000);
 
     var cursor: Cursor = Cursor.init();
 
-    const Self = @This();
+    var current_colour: ColourCode = ColourCode.init(.light_grey, .black);
 
-    const Error = error{};
-
-    pub const Writer = std.io.Writer(*Self, Error, terminalWriter); 
-
-    pub fn init() Terminal {
-        return Terminal {
-            .buffer = @intToPtr([*]volatile ScreenChar, 0xB8000)[0..VGA_SIZE],
-        };
-    }
-
-    fn putCharAt(self: *Self, screen_char: u8, x: usize, y: usize) void {
+    fn putCharAt(screen_char: u8, x: usize, y: usize) void {
         if (screen_char == '\n') {
             cursor.returnLine();
 
@@ -42,42 +64,42 @@ pub const Terminal = struct {
 
         const index = y * VGA_WIDTH + x;
 
-        self.buffer[index].char = screen_char;
+        buffer[index].char = screen_char;
 
-        self.buffer[index].colour_code = self.current_colour;
+        buffer[index].colour_code = current_colour;
 
         cursor.pos += 1;
     }
 
-    fn putChar(self: *Self, screen_char: u8) void {
+    fn putChar(screen_char: u8) void {
         if (cursor.row() >= VGA_HEIGHT - 1) {
-            self.scroll();
+            scroll();
         }
 
-        self.putCharAt(screen_char, cursor.column(), cursor.row());
+        putCharAt(screen_char, cursor.column(), cursor.row());
 
         if (cursor.column() == VGA_WIDTH) {
             cursor.returnLine();
         }
     }
 
-    fn write(self: *Self, msg: []const u8) void {
+    fn write(msg: []const u8) void {
         for (msg) |c| {
-            self.putChar(c);
+            putChar(c);
         }
 
         cursor.move();
     }
 
-    fn writeln(self: *Self, msg: []const u8) void {
+    fn writeln(msg: []const u8) void {
         for (msg) |c| {
-            self.putChar(c);
+            putChar(c);
         }
 
-        self.putChar('\n');
+        putChar('\n');
     }
 
-    fn scroll(self: *Self) void {
+    fn scroll() void {
         const first_row: usize = VGA_WIDTH;
 
         // The last row as an array index
@@ -86,58 +108,48 @@ pub const Terminal = struct {
         // The last row as per 0..VGA_HEIGHT
         const last_row_index: usize = VGA_HEIGHT - 1;
 
-        // Copy the second row..last row (2nd param) to the first row..penultimate_row (1st param)
-        std.mem.copy(ScreenChar, self.buffer[0..last_row], self.buffer[first_row..VGA_SIZE]);
+        const buffer_array = bufferAsSlice();
+        
+        // Copy the second row..last row (2nd param) to the first row..last_row (1st param)
+        std.mem.copy(ScreenChar, buffer_array[0..last_row], buffer_array[first_row..VGA_SIZE]);
 
         cursor.pos -= VGA_WIDTH;
 
         cursor.move();
     }
 
-    pub fn clear(self: *Self, colour_code: ?ColourCode) void {
-        std.mem.set(ScreenChar, self.buffer, ScreenChar.init(' ', colour_code orelse self.current_colour));
+    pub fn clear(colour_code: ?ColourCode) void {
+        std.mem.set(ScreenChar, buffer[0..VGA_SIZE], ScreenChar.init(' ', colour_code orelse current_colour));
 
         cursor.pos = 0;
 
         cursor.move();
     }
 
-    fn writeRowAbsolute(self: *Self, screen_char: u8, row_write: usize) void {
+    fn writeRowAbsolute(screen_char: u8, row_write: usize) void {
         const row_start: usize = row_write * VGA_WIDTH;
 
         const row_end: usize = row_start + VGA_WIDTH;
 
         // Copy from the start of the row index to the end of the row
-        std.mem.set(ScreenChar, self.buffer[row_start..row_end], self.screenChar(screen_char));
+        std.mem.set(ScreenChar, bufferAsSlice()[row_start..row_end], screenChar(screen_char));
 
         cursor.returnLine();
     }
 
-    pub fn writeRow(self: *Self, screen_char: u8) void {
-        self.writeRowAbsolute(screen_char, cursor.row()); 
-    }
-
-    fn screenChar(self: *Self, char: u8) ScreenChar {
-        return ScreenChar.init(char, self.current_colour);
-    }
-
-    fn terminalWriter(self: *Self, msg: []const u8) Error!usize {
-        self.write(msg);
-
-        return msg.len;
-    }
-
-    pub fn writer(self: *Self) std.io.Writer(*Self, Error, terminalWriter) {
-        return Writer{ .context = self };
-    }
-
-    pub fn log(self: *Self, msg: []const u8) void {
-        self.writer().writeAll(msg) catch |e| switch (e) {};
-
+    fn returnLine() void {
         cursor.returnLine();
     }
 
-    pub fn print(self: *Self, comptime msg: []const u8, args: anytype) void {
-        self.writer().print(msg, args) catch |e| switch (e) {};
+    pub fn writeRow(screen_char: u8) void {
+        writeRowAbsolute(screen_char, cursor.row()); 
+    }
+
+    fn screenChar(char: u8) ScreenChar {
+        return ScreenChar.init(char, current_colour);
+    }
+
+    fn bufferAsSlice() []ScreenChar {
+        return buffer[0..VGA_SIZE];
     }
 };
